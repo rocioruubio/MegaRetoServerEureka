13d42caf1fb8318d1c9f87453d4947a3222701b4
Rocío <rocioorubio17@gmail.com>
2021-11-18 14:50:41 +0100

    PRIMERA SUBIDA

---
+++ src/test/java/com/ntt/megareto/MegaRetoServerEureka/MegaRetoServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntt.megareto.MegaRetoServerEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaRetoServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
